074c417d496505fa374dcf6c8849e70a0e41e94a
Shadab khan <112720913+Shadabkhan0809@users.noreply.github.com>
2022-12-28 12:08:24 +0530

    a
    A

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
 import {fetchMovieAvailability,fetchMovieList} from "./api.js"
+
+
+fetchMovieList()
+.then((response)=>{
+    document.getElementById("loader").remove(); // remove loader
+    const movieHolder=document.createElement("div")
+    movieHolder.classList.add('movie-holder')
+    let moviesContent=""
+    response.forEach((element)=>{
+        moviesContent += `<a class="movie- link" href="/${element.name}">
+        <div class="movie" data- d="moviename">
+        <div class="movie-img-wrapper" style="backgroud: url('${element.imgUrl}')">
+        </div>
+        <h4>${element.name}</h4>
+        </div>
+        </a>
+        ` 
+    })
+    movieHolder.innerHTML = moviesContent
+    document.querySelector("main").appendChild(movieHolder)
+    const moviesItem=document.getElementsByClassName("movie-link")
+    for (let i=0;i<moviesItem;i++){
+        moviesItem[i].addEventListener("click",handleMovieBox)
+    }
+    console.log(response);
+    
+
+})
+.catch((error)=>{
+    console.log(error);
+})
+
+
+const handleMovieBox=(e)=>{
+    e.preventDefoult()
+  console.log(e);
+}
